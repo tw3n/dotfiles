c25dfe8f50e8e6a53ea1e6855bba6985846e5fb5
Xavier Hurst <xavier@hurst.im>
2021-10-24 12:34:57 +0200

    fish: add kubie segment

---
+++ fish/functions/__flseg_kubie.fish
@@ -0,0 +1,14 @@
+function __flseg_kubie
+    if set -q KUBIE_ACTIVE
+        set -l ctx  (kubie info ctx)
+        set -l ns (kubie info ns)
+
+        set -l out "$ctx"
+        if [ "$ns" != "default" ]
+            set out "$ctx($ns)"
+        end
+
+        __fishline_segment 66cccc 393939
+        printf "\ufd31 %s" "$out"
+    end
+end
