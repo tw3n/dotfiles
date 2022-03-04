d420bd915300fab9208b837d80fd7d329e9f7ce5
Xavier Hurst <xavier@hurst.im>
2022-03-04 16:08:41 +0100

    fish: update awsprofile segment color

--- fish/functions/__flseg_awsprofile.fish
+++ fish/functions/__flseg_awsprofile.fish
@@ -1,6 +1,6 @@
 function __flseg_awsprofile
     if set -q AWS_PROFILE
-        __fishline_segment ffcc66 393939
+        __fishline_segment d18ff0 393939
         printf "\uf0c2 %s" "$AWS_PROFILE($AWS_REGION)"
     end
 end
