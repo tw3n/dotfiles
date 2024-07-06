d507972eddf578192b541b6a67b0d6abf78dcee8
Xavier Hurst <xavier@hurst.im>
2024-07-06 08:27:02 +0200

    fish: fix kubie segment

--- fish/functions/__flseg_kubie.fish
+++ fish/functions/__flseg_kubie.fish
@@ -2,13 +2,13 @@ function __flseg_kubie
     if set -q KUBIE_ACTIVE
         set -l ctx  (kubie info ctx)
         set -l ns (kubie info ns)
-
         set -l out "$ctx"
+
         if [ "$ns" != "default" ]
             set out "$ctx($ns)"
         end
 
         __fishline_segment 66cccc 393939
-        printf "\ufd31 %s" "$out"
+        printf "\u2388 %s" "$out"
     end
 end
